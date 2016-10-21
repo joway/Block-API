0dbd429e90813e6d93bb13513bc6ba6c37c68be5
joway <joway.w@gmail.com>
2016-10-21 23:49:04 +0800

    add migrate

--- comment/migrations/0002_comment_author.py
+++ comment/migrations/0002_comment_author.py
@@ -13,13 +13,13 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('social', '0001_initial'),
+        ('comment', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='socialaccount',
-            name='user',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            model_name='comment',
+            name='author',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
